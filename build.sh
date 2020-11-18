ebb616782c1ea873880056f98b846042807df48d
Ionel-Cristinel ANICHITEI <ianichitei@bitdefender.com>
2020-11-18 14:46:00 +0200

    Also test that find_package works

--- build.sh
+++ build.sh
@@ -1,23 +1,21 @@
 set -e
 
-cd bddisasm
+echo "\n[+] Building and installing bddisasm..."
+cmake -B bddisasm/build bddisasm
+cmake --build bddisasm/build
+sudo cmake --build bddisasm/build --target install
 
-mkdir -p build
-cd build
+echo "\n[+] Checking pkg-config..."
+pkg-config --cflags --libs libbddisasm
 
-cmake ..
-sudo make install
+echo "\n[+] Building the samples using pkg-config to find bddisasm and bdshemu..."
+cmake -B build/pkgconfig . -DUSE_PKGCONFIG=ON
+cmake --build build/pkgconfig
+./build/pkgconfig/bddisasm_sample
+./build/pkgconfig/bdshemu_sample
 
-cd ..
-cd ..
-
-mkdir -p build
-cd build
-
-cmake ..
-make
-
-./bddisasm_sample
-./bdshemu_sample
-
-cd ..
+echo "\n[+] Building the samples without using pkg-config to find bddisasm and bdshemu..."
+cmake -B build/package . -DUSE_PKGCONFIG=OFF
+cmake --build build/package
+./build/package/bddisasm_sample
+./build/package/bdshemu_sample
