26db2002dfca348ba427cbb852bd5e2afdbf463f
Ionel-Cristinel ANICHITEI <ianichitei@bitdefender.com>
2020-11-18 17:31:45 +0200

    Add vcpkg test

---
+++ build.cmd
@@ -0,0 +1,41 @@
+@echo off
+
+echo [+] Setting up vcpkg...
+call vcpkg\bootstrap-vcpkg.bat
+IF NOT %ERRORLEVEL% == 0 goto :failure
+call vcpkg\vcpkg integrate install
+IF NOT %ERRORLEVEL% == 0 goto :failure
+call vcpkg\vcpkg install bddisasm:x86-windows
+IF NOT %ERRORLEVEL% == 0 goto :failure
+call vcpkg\vcpkg install bddisasm:x64-windows
+IF NOT %ERRORLEVEL% == 0 goto :failure
+
+echo [+] Building for Debug x64...
+call msbuild msvc\bddisasm_user\bddisasm_user.sln /t:Rebuild /p:Configuration=Debug /p:Platform=x64 /maxcpucount
+IF NOT %ERRORLEVEL% == 0 goto :failure
+call msbuild msvc\bdshemu_user\bdshemu_user.sln /t:Rebuild /p:Configuration=Debug /p:Platform=x64 /maxcpucount
+IF NOT %ERRORLEVEL% == 0 goto :failure
+
+echo [+] Building for Release x64...
+call msbuild msvc\bddisasm_user\bddisasm_user.sln /t:Rebuild /p:Configuration=Release /p:Platform=x64 /maxcpucount
+IF NOT %ERRORLEVEL% == 0 goto :failure
+call msbuild msvc\bdshemu_user\bdshemu_user.sln /t:Rebuild /p:Configuration=Release /p:Platform=x64 /maxcpucount
+IF NOT %ERRORLEVEL% == 0 goto :failure
+
+echo [+] Building for Debug x86...
+call msbuild msvc\bddisasm_user\bddisasm_user.sln /t:Rebuild /p:Configuration=Debug /p:Platform=x86 /maxcpucount
+IF NOT %ERRORLEVEL% == 0 goto :failure
+call msbuild msvc\bdshemu_user\bdshemu_user.sln /t:Rebuild /p:Configuration=Debug /p:Platform=x86 /maxcpucount
+IF NOT %ERRORLEVEL% == 0 goto :failure
+
+echo [+] Building for Release x86...
+call msbuild msvc\bddisasm_user\bddisasm_user.sln /t:Rebuild /p:Configuration=Release /p:Platform=x86 /maxcpucount
+IF NOT %ERRORLEVEL% == 0 goto :failure
+call msbuild msvc\bdshemu_user\bdshemu_user.sln /t:Rebuild /p:Configuration=Release /p:Platform=x86 /maxcpucount
+IF NOT %ERRORLEVEL% == 0 goto :failure
+
+echo Build Done!
+exit /b 0
+
+:failure
+exit /b %ERRORLEVEL%
--- build.sh
+++ build.sh
@@ -6,6 +6,10 @@ cmake --build build/subdir
 ./build/subdir/bddisasm_sample
 ./build/subdir/bdshemu_sample
 
+# Cleanup the bddisasm build dirs
+rm -rf bin
+rm -rf bddisasm/build
+
 echo "\n[+] Building and installing bddisasm..."
 cmake -B bddisasm/build bddisasm
 cmake --build bddisasm/build
@@ -25,3 +29,22 @@ cmake -B build/package . -DUSE_PKGCONFIG=OFF
 cmake --build build/package
 ./build/package/bddisasm_sample
 ./build/package/bdshemu_sample
+
+echo "\n[+] Setting up vcpkg..."
+cd vcpkg
+./bootstrap-vcpkg.sh
+./vcpkg install bddisasm
+cd -
+
+echo "\n[+] Building the samples using vcpkg on Debug..."
+cmake -B build/vcpkg/Debug . -DUSE_PKGCONFIG=OFF -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake --build build/vcpkg/Debug
+./build/vcpkg/Debug/bddisasm_sample
+./build/vcpkg/Debug/bdshemu_sample
+
+echo "\n[+] Building the samples using vcpkg on Release..."
+cmake -B build/vcpkg/Release . -DUSE_PKGCONFIG=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake --build build/vcpkg/Release
+./build/vcpkg/Release/bddisasm_sample
+./build/vcpkg/Release/bdshemu_sample
