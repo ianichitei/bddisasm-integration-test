e853f54150017b5c767bc606bfb1c4363fe4c514
Ionel-Cristinel ANICHITEI <ianichitei@bitdefender.com>
2021-03-30 21:59:46 +0300

    Handle BDDISASM_HAS_VSNPRINTF and BDDISASM_HAS_MEMSET

--- common/common.c
+++ common/common.c
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+#if !defined(BDDISASM_HAS_VSNPRINTF)
 int nd_vsnprintf_s(char *buffer, size_t sizeOfBuffer, size_t count, const char *format, va_list argptr)
 {
     return vsnprintf(buffer, sizeOfBuffer, format, argptr);
 }
+#endif // !defined(BDDISASM_HAS_VSNPRINTF)
 
+#if !defined(BDDISASM_HAS_MEMSET)
 void *nd_memset(void *s, int c, size_t n)
 {
     return memset(s, c, n);
 }
+#endif // !!defined(BDDISASM_HAS_MEMSET)
