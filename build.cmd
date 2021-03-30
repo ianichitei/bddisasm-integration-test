0cae35993bbcb47de0b3068d182e2d1ad41b4c2e
Ionel-Cristinel ANICHITEI <ianichitei@bitdefender.com>
2021-03-30 16:34:29 +0300

    Use Ninja on Windows

--- build.cmd
+++ build.cmd
@@ -1,31 +1,31 @@
 @echo off
 
 REM As a subdirectory.
-cmake -B build/subdir . -DUSE_AS=subdir
+cmake -B build/subdir . -DUSE_AS=subdir -G Ninja -DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=cl
 if %errorlevel% neq 0 exit /b %errorlevel%
 cmake --build build/subdir --config Release
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM With an installed copy.
 echo "Building and installing bddisasm..."
-cmake -B bddisasm/build bddisasm -DCMAKE_INSTALL_PREFIX=./install-dir
+cmake -B bddisasm/build bddisasm -DCMAKE_INSTALL_PREFIX=./install-dir -G Ninja -DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=cl
 if %errorlevel% neq 0 exit /b %errorlevel%
 cmake --build bddisasm/build --target install
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-cmake -B build/installed . -DUSE_AS=installed -DBDDISASM_INSTALL_PATH=./install-dir
+cmake -B build/installed . -DUSE_AS=installed -DBDDISASM_INSTALL_PATH=./install-dir -G Ninja -DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=cl
 if %errorlevel% neq 0 exit /b %errorlevel%
 cmake --build build/installed --config Release
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM With an installed copy in a path with spaces.
 echo "Building and installing bddisasm in a path with spaces..."
-cmake -B bddisasm/build_spaces bddisasm -DCMAKE_INSTALL_PREFIX="./install dir"
+cmake -B bddisasm/build_spaces bddisasm -DCMAKE_INSTALL_PREFIX="./install dir" -G Ninja -DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=cl
 if %errorlevel% neq 0 exit /b %errorlevel%
 cmake --build bddisasm/build_spaces --target install
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-cmake -B build/installed_spaces . -DUSE_AS=installed -DBDDISASM_INSTALL_PATH="./install dir"
+cmake -B build/installed_spaces . -DUSE_AS=installed -DBDDISASM_INSTALL_PATH="./install dir" -G Ninja -DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=cl
 if %errorlevel% neq 0 exit /b %errorlevel%
 cmake --build build/installed_spaces --config Release
 if %errorlevel% neq 0 exit /b %errorlevel%
